1cfe01d32ca071adc591c33ad79434b278fc0340
Tiltovskii <benderabenderabendera@gmail.com>
2025-12-21 00:13:06 +0300

    add BeamSearchCERMetric and BeamSearchWERMetric

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -1,2 +1,9 @@
-from src.metrics.cer import ArgmaxCERMetric
-from src.metrics.wer import ArgmaxWERMetric
+from src.metrics.cer import ArgmaxCERMetric, BeamSearchCERMetric
+from src.metrics.wer import ArgmaxWERMetric, BeamSearchWERMetric
+
+__all__ = [
+    "ArgmaxCERMetric",
+    "BeamSearchCERMetric",
+    "ArgmaxWERMetric",
+    "BeamSearchWERMetric",
+]
--- src/metrics/cer.py
+++ src/metrics/cer.py
@@ -27,3 +27,25 @@ class ArgmaxCERMetric(BaseMetric):
             pred_text = self.text_encoder.ctc_decode(log_prob_vec[:length])
             cers.append(calc_cer(target_text, pred_text))
         return sum(cers) / len(cers)
+
+
+class BeamSearchCERMetric(BaseMetric):
+    def __init__(self, text_encoder, beam_size=10, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.text_encoder = text_encoder
+        self.beam_size = beam_size
+
+    def __call__(
+        self, log_probs: Tensor, log_probs_length: Tensor, text: List[str], **kwargs
+    ):
+        cers = []
+        probs = torch.exp(log_probs.cpu())
+        lengths = log_probs_length.detach().numpy()
+        for prob_vec, length, target_text in zip(probs, lengths, text):
+            target_text = self.text_encoder.normalize_text(target_text)
+            beam_results = self.text_encoder.ctc_beam_search(
+                prob_vec[:length], self.beam_size
+            )
+            pred_text = beam_results[0][0]
+            cers.append(calc_cer(target_text, pred_text))
+        return sum(cers) / len(cers)
